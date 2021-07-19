641c2a75009c9823573cf80201e44bd322d5cd05
HyeonJaeGil <now9728@kaist.ac.kr>
2021-07-20 01:18:45 +0900

    tested with ocam-1mgn-u-t

--- src/save_video.cpp
+++ src/save_video.cpp
@@ -19,7 +19,7 @@ int main(){
   // cap(file_name); 
 
   // open webcam device
-  int deviceID = 4;
+  int deviceID = 6;
   int apiID = cv::CAP_V4L2;
   cap.open(deviceID, apiID);
 
@@ -38,11 +38,18 @@ int main(){
   cap.set(cv::CAP_PROP_FRAME_WIDTH, 640);
   cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
   cap.set(cv::CAP_PROP_FPS, 30);
+  // cap.set(cv::CAP_PROP_MONOCHROME, true);
 
 
   while(1){
 
     static int frame_count = 0;
+
+    if (frame_count < 5)
+    {
+      frame_count ++;
+      continue;
+    }
 
     std::unique_ptr<cv::Mat> frame_ptr = std::make_unique<cv::Mat>();
     // Capture frame-by-frame
@@ -53,7 +60,7 @@ int main(){
       break;
 
     // cout << save_path + to_string(frame_count) + ".png" << endl;
-    imwrite (save_path + to_string(frame_count) + ".png", *frame_ptr);
+    cv::imwrite (save_path + to_string(frame_count) + ".png", *frame_ptr);
 
     // Press  ESC on keyboard to exit
     char c=(char)waitKey(25);
